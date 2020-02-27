bad172241d5e8e3bfeb486d29a68c6363da298ff
ignacioct <ignaciotalaveracepeda@gmail.com>
2020-02-28 00:52:09 +0100

    erased

--- bot/keys.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'iaQzqQugvpEPwMlnn7kOVoFpG'
-consumer_secret = '08I0uNtaz12NhsmAkZuzpB5lf6UPktdaMRyMfJJ6oeYA2STjeJ'
-access_token = '1233110563485634560-4L0HOcRUx6Kl9CSrQqcWULqTIweVw3'
-access_token_secret = 'rlMrdI2aRivzuTjD9EXLuKDxrKQuOq0AFZJiAzK9rrUbZ'
